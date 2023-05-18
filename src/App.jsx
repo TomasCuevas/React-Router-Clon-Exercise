4b71d86168be6aeaa88f2607100bcacadab175b9
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-17 23:24:50 -0300

    Navegación SPA programática

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const NAVIGATION_EVENT = "pushstate";
+
+function navigate(href) {
+  window.history.pushState({}, "", href);
+
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
+}
 
 export const HomePage = () => {
   return (
     <>
       <h1>Home</h1>
       <p>Esta es una página de ejemplo</p>
-      <a href="/about">Ir a Sobre Nosotros</a>
+      <button onClick={() => navigate("/about")}>Ir a Sobre Nosotros</button>
     </>
   );
 };
@@ -23,13 +32,23 @@ export const AboutPage = () => {
       <p>
         Hola! Me llamo Tomás Cuevas y estoy creando un clon de React Router.
       </p>
-      <a href="/">Ir a Home</a>
+      <button onClick={() => navigate("/")}>Ir a Home</button>
     </>
   );
 };
 
 export const App = () => {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+
+    return () => window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+  }, []);
 
   return (
     <main>
